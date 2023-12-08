1ed7e7bc89a5a9ed995b2c1ff22d49386c5dd511
ET-SAPRK <samuelwoyesso2016@gmail.com>
2023-12-08 03:13:33 -0800

    edited

---
+++ components/FeaturedCourses.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1 class="font-bold text-2xl ml-16 mt-28">Featured Courses</h1>
+    <p class="text-gray-500 ml-16 mt-4">
+      High quality selected courses to help you learn a skill you need
+    </p>
+  </div>
+  <div class="ml-16 mt-8 border-2 border-gray-200 w-1/4 mb-24">
+    <div>
+      <img
+        src="https://www.cypherlearning.com/hubfs/Imported_Blog_Media/A-teachers-guide-to-becoming-an-online-course-creator.jpg"
+      />
+    </div>
+    <div class="border-t-2 px-3">
+      <div>
+        <p class="text-xl">
+          Learn English: Upgrade Your Speaking and Listening Skill
+        </p>
+      </div>
+      <div class="text-sm">cource by : Abebe</div>
+      <div>
+        <p class="text-gray-500 text-sm">
+          Are you struggling to communicate in English? We understand that it
+          can be difficult to speak naturally in a new
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-between border-t-2 px-3 mt-12 items-center">
+      <div class="text-center">
+        <div class="text-gray-500">
+          <Icon
+            name="carbon:skill-level-basic"
+            class="bg-white cursor-pointer mr-1"
+            color="#212F3D"
+          />Level
+        </div>
+        <div class="font-bold">Intermediate</div>
+      </div>
+      <div class="text-center">
+        <div class="text-gray-500">
+          <Icon
+            name="mdi:clock-time-eleven"
+            class="bg-white cursor-pointer mr-1"
+            color="#212F3D"
+          />Duration
+        </div>
+        <div class="font-bold">4:00 h</div>
+      </div>
+      <div class="text-center">
+        <div class="text-gray-500">
+          <Icon
+            name="material-symbols:play-lesson"
+            class="bg-white cursor-pointer mr-1"
+            color="#212F3D"
+          />lessons
+        </div>
+        <div class="font-bold">36</div>
+      </div>
+    </div>
+    <div class="flex justify-center border-t-2 px-3 py-2">
+      <button
+        class="border border-blue-500 hover:border-blue-600 text-blue-500 hover:text-white px-4 py-4 rounded-s w-full"
+      >
+        Get started
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
